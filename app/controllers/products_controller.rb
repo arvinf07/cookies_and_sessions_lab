0346a8eb0d30d88a9a74baec3e2bf5b562ea8231
Arvin Fernandez <arvinf07@gmail.com>
2021-01-14 19:47:20 -0500

    commit tree b5f2577518bfc081cab061faa4f85a96a582fbe4

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+
+  def index
+
+  end
+  
+  def add
+    if session[:cart]
+      session[:cart] << params[:product]
+    else
+      session[:cart] = []
+      session[:cart] << params[:product]
+    end  
+    redirect_to '/'
+  end  
+
+end
